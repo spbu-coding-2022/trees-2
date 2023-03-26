a743363348aa80fd165c5c8124e90afd28f53e99
Efremov Alexey <dragonmail135159@gmail.com>
2023-03-26 22:16:50 +0300

    feat(TemplateBSTree): add remove() method and related methods

--- binaryTree/src/main/kotlin/org/tree/binaryTree/TemplateBSTree.kt
+++ binaryTree/src/main/kotlin/org/tree/binaryTree/TemplateBSTree.kt
@@ -58,6 +58,65 @@ abstract class TemplateBSTree<T : Comparable<T>, NODE_T : TemplateNode<T, NODE_T
         return find(root, obj)
     }
 
+    //Remove
+    private fun replaceDeletedNode(deletedNode: NODE_T, parentNode: NODE_T?, replaceNode: NODE_T?) {
+        if (parentNode == null) {
+            if (root === deletedNode) {
+                root = replaceNode
+            } else {
+                throw IllegalArgumentException("Received a non-root node with a null parent")
+            }
+        } else {
+            if (parentNode.right == deletedNode) {
+                parentNode.right = replaceNode
+            } else if (parentNode.left == deletedNode) {
+                parentNode.left = replaceNode
+            } else {
+                throw IllegalArgumentException("Received a node with a wrong parent")
+            }
+        }
+    }
+
+    protected open fun deleteNode(curNode: NODE_T, parentNode: NODE_T?): Int {
+        val res = curNode.countNullChild()
+        when (res) {
+            0 -> {
+                val nxt =
+                    findNext(curNode) ?: throw IllegalArgumentException("Got null as next than right child isn't null")
+                val buf = nxt.elem
+                remove(nxt.elem)
+                curNode.elem = buf
+            }
+
+            1 -> {
+                replaceDeletedNode(curNode, parentNode, curNode.getNonNullChild())
+            }
+
+            else -> {
+                replaceDeletedNode(curNode, parentNode, null)
+            }
+        }
+        return res
+    }
+
+    protected open fun remove(curNode: NODE_T?, parentNode: NODE_T?, obj: T): Int? {
+        if (curNode == null) {
+            return null
+        }
+
+        if (curNode.elem == obj) {
+            return deleteNode(curNode, parentNode)
+        } else if (obj < curNode.elem) {
+            return remove(curNode.left, curNode, obj)
+        } else {
+            return remove(curNode.right, curNode, obj)
+        }
+    }
+
+    fun remove(obj: T) {
+        remove(root, null, obj)
+    }
+
     //Additional
     protected fun findNext(curNode: NODE_T): NODE_T? {
         var res = curNode.right
