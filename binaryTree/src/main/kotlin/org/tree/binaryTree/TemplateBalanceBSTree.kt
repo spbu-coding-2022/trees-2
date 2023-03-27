68931e0f480d97929ab936164bce2ec26c0d932c
Efremov Alexey <dragonmail135159@gmail.com>
2023-03-27 10:17:59 +0300

    feat(BalanceTree): Add balance() method

--- binaryTree/src/main/kotlin/org/tree/binaryTree/TemplateBalanceBSTree.kt
+++ binaryTree/src/main/kotlin/org/tree/binaryTree/TemplateBalanceBSTree.kt
@@ -3,4 +3,22 @@ package org.tree.binaryTree
 
 abstract class TemplateBalanceBSTree<T : Comparable<T>, NODE_T : TemplateNode<T, NODE_T>> :
     TemplateBSTree<T, NODE_T>() {
+
+    //Balance
+    protected class BalanceCase {
+        enum class Recursive { RECURSIVE_CALL, END }
+        enum class OpType { INSERT, REMOVE_0, REMOVE_1, REMOVE_2 }
+    }
+
+    protected fun getBalanceRemoveType(a: Int): BalanceCase.OpType {
+        return when (a) {
+            2 -> BalanceCase.OpType.REMOVE_2
+            1 -> BalanceCase.OpType.REMOVE_1
+            else -> BalanceCase.OpType.REMOVE_0
+        }
+    }
+
+    protected abstract fun balance(curNode: NODE_T, operationType: BalanceCase.OpType, recursive: BalanceCase.Recursive)
+
+
 }
