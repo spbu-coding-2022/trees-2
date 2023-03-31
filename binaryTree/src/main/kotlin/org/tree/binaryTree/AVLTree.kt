f53ebede715523b15c249e725d178fd3498c5a02
Anna-er <brungilda364@gmail.com>
2023-03-31 22:16:23 +0300

    feat(AVLTree): Add height() method and related methods

--- binaryTree/src/main/kotlin/org/tree/binaryTree/AVLTree.kt
+++ binaryTree/src/main/kotlin/org/tree/binaryTree/AVLTree.kt
@@ -1,6 +1,26 @@
 package org.tree.binaryTree
 
 class AVLTree<T : Comparable<T>> : TemplateBalanceBSTree<T, AVLNode<T>>() {
+    private fun height(avlNode: AVLNode<T>?): Int {
+        return avlNode?.height ?: 0
+    }
+
+    private fun bfactor(avlNode: AVLNode<T>?): Int {
+        return if (avlNode != null) {
+            height(avlNode.right)-height(avlNode.left)
+        } else {
+            0
+        }
+    }
+
+    private fun fixheight(avlNode: AVLNode<T>?) {
+        if (avlNode != null) {
+            val hl = height(avlNode.left)
+            val hr = height(avlNode.right)
+            avlNode.height = (if (hl > hr) hl else hr) + 1
+        }
+    }
+
     override fun insert(curNode: AVLNode<T>?, obj: T): AVLNode<T>? {
         return super.insertNode(curNode, AVLNode(obj))
     }
