a19e4e11f684ef346e1f1a3e5a4f194026174a27
David <parzivalwasd@gmail.com>
2023-04-03 20:58:48 +0300

    feat(RBTree): Add a method to find the parent for the new node

--- binaryTree/src/main/kotlin/org/tree/binaryTree/RBTree.kt
+++ binaryTree/src/main/kotlin/org/tree/binaryTree/RBTree.kt
@@ -1,6 +1,24 @@
 package org.tree.binaryTree
 
 class RBTree<T : Comparable<T>> : TemplateBalanceBSTree<T, RBNode<T>>(){
+    private fun findParentForNewNode(curNode: RBNode<T>?, obj: T): RBNode<T>? {
+        if (curNode == null) { // impossible, but check for null is needed
+            return null
+        }
+        if (obj > curNode.elem) {
+            if (curNode.right == null) {
+                return curNode
+            }
+            return findParentForNewNode(curNode.right, obj)
+        } else if (obj < curNode.elem) {
+            if (curNode.left == null) {
+                return curNode
+            }
+            return findParentForNewNode(curNode.left, obj)
+        } else {  // the element already exist
+            return null
+        }
+    }
     override fun insert(curNode: RBNode<T>?, obj: T): RBNode<T>? {
         TODO("Not yet implemented")
     }
