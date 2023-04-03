d20a0decbf1769171fe380e56a77d07935c98ba7
David <parzivalwasd@gmail.com>
2023-04-03 20:56:52 +0300

    feat(RBTree): Init RBTree class

---
+++ binaryTree/src/main/kotlin/org/tree/binaryTree/RBTree.kt
@@ -0,0 +1,11 @@
+package org.tree.binaryTree
+
+class RBTree<T : Comparable<T>> : TemplateBalanceBSTree<T, RBNode<T>>(){
+    override fun insert(curNode: RBNode<T>?, obj: T): RBNode<T>? {
+        TODO("Not yet implemented")
+    }
+
+    override fun balance(curNode: RBNode<T>?, operationType: BalanceCase.OpType, recursive: BalanceCase.Recursive) {
+        TODO("Not yet implemented")
+    }
+}
