5794ca6545c949f987212e7e08fb99a05a15b1c1
Efremov Alexey <dragonmail135159@gmail.com>
2023-04-05 21:27:41 +0300

    test(BinSearchTree): Add insert tests

--- binaryTree/src/test/kotlin/org/tree/binaryTree/BinSearchTreeTest.kt
+++ binaryTree/src/test/kotlin/org/tree/binaryTree/BinSearchTreeTest.kt
@@ -16,10 +16,85 @@ fun <T : Comparable<T>> generateBinSearchTree(objects: List<T?>): Node<T>? {
 }
 
 class BinSearchTreeTest {
-    @Test
-    fun rootInsertTest() {
-        val testTree = BinSearchTree<Int>()
-        testTree.insert(3)
-        assertEquals(testTree.root?.elem, 3)
+    val testTree = BinSearchTree<Int>()
+
+    @DisplayName("Insert tests")
+    @Nested
+    inner class InsertTests {
+        @Test
+        fun rootInsertTest() {
+            val ok = testTree.insert(3)
+
+            assertEquals(true, ok)
+            assertEquals(3, testTree.root?.elem)
+        }
+
+        @Test
+        fun leftInsertTest() {
+            testTree.root = generateBinSearchTree(
+                listOf(
+                    40,
+                )
+            )
+            val ok = testTree.insert(20)
+
+            assertEquals(true, ok)
+            assertEquals(20, testTree.root?.left?.elem)
+        }
+
+        @Test
+        fun rightInsertTest() {
+            testTree.root = generateBinSearchTree(
+                listOf(
+                    40,
+                )
+            )
+            val ok = testTree.insert(60)
+
+            assertEquals(true, ok)
+            assertEquals(60, testTree.root?.right?.elem)
+        }
+
+        @Test
+        fun leftRecInsertTest() {
+            testTree.root = generateBinSearchTree(
+                listOf(
+                    40,
+                    20, 60,
+                )
+            )
+            val ok = testTree.insert(21)
+
+            assertEquals(true, ok)
+            assertEquals(21, testTree.root?.left?.right?.elem)
+        }
+
+        @Test
+        fun rightRecInsertTest() {
+            testTree.root = generateBinSearchTree(
+                listOf(
+                    40,
+                    20, 60,
+                )
+            )
+            val ok = testTree.insert(45)
+
+            assertEquals(true, ok)
+            assertEquals(45, testTree.root?.right?.left?.elem)
+        }
+
+        @Test
+        fun sameInsertTest() {
+            testTree.root = generateBinSearchTree(
+                listOf(
+                    40,
+                    null, 60,
+                    null, null, 45, null,
+                )
+            )
+            val ok = testTree.insert(45)
+
+            assertEquals(false, ok)
+        }
     }
 }
