a8261f9e300c42b2534f456679f8d4f7480e9f17
Efremov Alexey <dragonmail135159@gmail.com>
2023-04-05 19:41:45 +0300

    test: Add generateTree() fun

---
+++ binaryTree/src/test/kotlin/org/tree/binaryTree/TreeGenerator.kt
@@ -0,0 +1,28 @@
+package org.tree.binaryTree
+
+import java.util.*
+import kotlin.math.log2
+
+fun <T, NODE_T : TemplateNode<T, NODE_T>> generateTree(nodes: List<NODE_T?>): NODE_T? {
+    val fheight = log2((nodes.size + 1).toFloat())
+    val height = fheight.toInt()
+    if (fheight - height > 0.0) {
+        throw IllegalArgumentException("Received an invalid array to create a tree. Size of array should be equal (2**N)-1")
+    }
+
+    val q: Queue<NODE_T?> = LinkedList<NODE_T?>()
+    val root = nodes[0]
+    q.add(root)
+    var i = 1
+    while (i < nodes.size) {
+        val cur = q.poll()
+        cur?.left = nodes[i]
+        i++
+        q.add(cur?.left)
+        cur?.right = nodes[i]
+        i++
+        q.add(cur?.right)
+    }
+    return root
+}
+
