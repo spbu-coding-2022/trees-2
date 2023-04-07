f8473effc35bb9f8491f65f71b2b65d5f5622702
Efremov Alexey <dragonmail135159@gmail.com>
2023-04-07 20:45:53 +0300

    test: Add tree generator

---
+++ binaryTree/src/test/kotlin/org/tree/binaryTree/Util.kt
@@ -0,0 +1,41 @@
+package org.tree.binaryTree
+
+import java.util.*
+import kotlin.math.log2
+
+
+// Generate
+fun <T, NODE_T : TemplateNode<T, NODE_T>> generateTree(nodes: List<NODE_T?>): NODE_T? {
+    val fheight = log2((nodes.size + 1).toFloat())
+    val height = fheight.toInt()
+    if (fheight - height > 0.0) {
+        throw IllegalArgumentException("Received an invalid array to create a tree. Size of array should be equal (2**N)-1")
+    }
+
+    val q: Queue<NODE_T?> = LinkedList<NODE_T?>()
+    val root = nodes[0]
+    q.add(root)
+    var i = 1
+    while (i < nodes.size) {
+        val cur = q.poll()
+        cur?.left = nodes[i]
+        i++
+        q.add(cur?.left)
+        cur?.right = nodes[i]
+        i++
+        q.add(cur?.right)
+    }
+    return root
+}
+
+fun <T : Comparable<T>> generateNodeTree(objects: List<T?>): Node<T>? {
+    val new = objects.map {
+        if (it != null) {
+            Node(it)
+        } else {
+            null
+        }
+    }
+    return generateTree(new)
+}
+
