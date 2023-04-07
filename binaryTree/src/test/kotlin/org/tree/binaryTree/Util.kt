ebcac8e2ec43b6b0c94c0dc5290960cd141be828
Efremov Alexey <dragonmail135159@gmail.com>
2023-04-07 20:47:18 +0300

    test: Add tree check fun

--- binaryTree/src/test/kotlin/org/tree/binaryTree/Util.kt
+++ binaryTree/src/test/kotlin/org/tree/binaryTree/Util.kt
@@ -1,5 +1,7 @@
 package org.tree.binaryTree
 
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.*
 import java.util.*
 import kotlin.math.log2
 
@@ -39,3 +41,27 @@ fun <T : Comparable<T>> generateNodeTree(objects: List<T?>): Node<T>? {
     return generateTree(new)
 }
 
+// Check
+fun <T : Comparable<T>, NODE_T : TemplateNode<T, NODE_T>> checkTreeNode(curNode: NODE_T?) {
+    if (curNode != null) {
+        val l = curNode.left
+        if (l != null) {
+            assertThat(curNode.elem, greaterThanOrEqualTo(l.elem))
+            checkTreeNode(l)
+        }
+
+        val r = curNode.right
+        if (r != null) {
+            assertThat(curNode.elem, lessThanOrEqualTo(r.elem))
+            checkTreeNode(r)
+        }
+    }
+}
+
+fun <T : Comparable<T>, NODE_T : TemplateNode<T, NODE_T>> checkBinSearchTree(
+    tree: TemplateBSTree<T, NODE_T>,
+    contain: Array<T>
+) {
+    assertThat(tree.traversal(TemplateNode.Traversal.INORDER), containsInAnyOrder(*contain))
+    checkTreeNode(tree.root)
+}
