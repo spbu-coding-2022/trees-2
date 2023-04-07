0c2853820581e06f4dfd2782c3b8151f32f03626
Efremov Alexey <dragonmail135159@gmail.com>
2023-04-07 20:48:05 +0300

    test(BinSearchTree): Add insert() tests

--- binaryTree/src/test/kotlin/org/tree/binaryTree/BinSearchTreeTest.kt
+++ binaryTree/src/test/kotlin/org/tree/binaryTree/BinSearchTreeTest.kt
@@ -1,14 +1,77 @@
 package org.tree.binaryTree
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
-import org.tree.binaryTree.trees.BinSearchTree
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.*
+import org.junit.jupiter.api.*
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
 class BinSearchTreeTest {
-    @Test
-    fun rootInsertTest() {
-        val testTree = BinSearchTree<Int>()
-        testTree.insert(3)
-        assertEquals(testTree.root?.elem, 3)
+    @DisplayName("insert() tests")
+    class InsertTests {
+        @ParameterizedTest(name = "[{index}]: tree = {0}, insert = {1}")
+        @MethodSource("testInsertArgs")
+        fun testInsert(values: List<Int?>, insVal: Int) {
+            val tree = BinSearchTree<Int>()
+            tree.root = generateNodeTree(values)
+            val valuesSet = values.filterNotNull().toMutableSet()
+            val exp = valuesSet.add(insVal)
+            val act = tree.insert(insVal)
+
+            assertThat(act, equalTo(exp))
+            checkBinSearchTree(tree, valuesSet.toTypedArray())
+        }
+
+        companion object {
+            @JvmStatic
+            fun testInsertArgs(): Stream<Arguments> {
+                return Stream.of(
+                    //[1] root insert
+                    Arguments.of(listOf(null), 40),
+                    //[2] left insert
+                    Arguments.of(
+                        listOf(
+                            40,
+                        ),
+                        20
+                    ),
+                    //[3] right insert
+                    Arguments.of(
+                        listOf(
+                            40,
+                        ),
+                        60
+                    ),
+                    //[4] left rec insert
+                    Arguments.of(
+                        listOf(
+                            40,
+                            20, 60
+                        ),
+                        21
+                    ),
+                    //[5] right rec insert
+                    Arguments.of(
+                        listOf(
+                            40,
+                            20, 60
+                        ),
+                        55
+                    ),
+                    //[6] same insert
+                    Arguments.of(
+                        listOf(
+                            40,
+                            null, 60,
+                            null, null, 45, null,
+                        ),
+                        45
+                    ),
+                )
+            }
+        }
     }
 }
