dfd364de348345131b0f8407f596da9d12f1ede7
Efremov Alexey <dragonmail135159@gmail.com>
2023-04-10 14:59:47 +0300

    test(RBTree): Add insert() tests

--- binaryTree/src/test/kotlin/org/tree/binaryTree/RBTreeTest.kt
+++ binaryTree/src/test/kotlin/org/tree/binaryTree/RBTreeTest.kt
@@ -10,6 +10,49 @@ import java.util.stream.Stream
 import kotlin.random.Random
 
 class RBTreeTest {
+    @DisplayName("insert() tests")
+    class InsertTests {
+        @ParameterizedTest(name = "[{index}]: insertCount = {1}, seed = {0}")
+        @MethodSource("testInsertArgs")
+        fun testInsert(seed: Long, insertCount: Int) {
+            val tree = RBTree<Int>()
+            val values = mutableSetOf<Int>()
+            val randomizer = Random(seed)
+
+            for (i in 1..insertCount) {
+                val newVal = randomizer.nextInt()
+                val exp = values.add(newVal)
+                val act = tree.insert(newVal)
+                //println("Insert $newVal")
+
+                MatcherAssert.assertThat(act, Matchers.equalTo(exp))
+                checkRBTree(tree, values.toTypedArray())
+            }
+
+
+        }
+
+        companion object {
+            @JvmStatic
+            fun testInsertArgs(): Stream<Arguments> {
+                return Stream.of(
+                    genArguments(0xdeadbeef),
+                    genArguments(0xabacaba, 10),
+                    genArguments(42),
+                    genArguments(13),
+                    genArguments(0xcafe),
+                    genArguments(1337),
+                )
+            }
+
+            private fun genArguments(seed: Long, insertCount: Int = 1000): Arguments {
+                return Arguments.of(seed, insertCount)
+            }
+        }
+
+    }
+
+
     @DisplayName("remove() tests")
     class RemoveTests {
         @ParameterizedTest(name = "[{index}]: treeSize = {1}, removeCount = {2}, seed = {0}")
