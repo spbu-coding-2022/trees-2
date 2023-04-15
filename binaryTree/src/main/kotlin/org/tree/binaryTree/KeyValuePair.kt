96da1fed852244b422583f2348f32a9038282d49
Efremov Alexey <dragonmail135159@gmail.com>
2023-04-15 14:25:17 +0300

    feat: Add KeyValuePair class

---
+++ binaryTree/src/main/kotlin/org/tree/binaryTree/KeyValuePair.kt
@@ -0,0 +1,7 @@
+package org.tree.binaryTree
+
+class KVP<K : Comparable<K>, V>(val key: K, var v: V?) : Comparable<KVP<K, V>> {
+    override fun compareTo(other: KVP<K, V>): Int {
+        return key.compareTo(other.key)
+    }
+}
