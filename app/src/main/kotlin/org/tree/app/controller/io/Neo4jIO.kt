e7a99fa72c8e2355e44f80072bff9c8d922a4fbc
Efremov Alexey <dragonmail135159@gmail.com>
2023-04-16 10:44:34 +0300

    feat(Neo4jIO): Add exportRBNode() fun

--- app/src/main/kotlin/org/tree/app/controller/io/Neo4jIO.kt
+++ app/src/main/kotlin/org/tree/app/controller/io/Neo4jIO.kt
@@ -3,12 +3,54 @@ package org.tree.app.controller.io
 import org.neo4j.driver.AuthTokens
 import org.neo4j.driver.Driver
 import org.neo4j.driver.GraphDatabase
+import org.neo4j.driver.TransactionContext
 import org.neo4j.driver.exceptions.SessionExpiredException
+import org.tree.app.view.NodeView
+import org.tree.binaryTree.KVP
+import org.tree.binaryTree.RBNode
 import java.io.Closeable
 import java.io.IOException
 
 class Neo4jIO() : Closeable {
     private var driver: Driver? = null
+
+    private fun exportRBNode(
+        nodeView: NodeView<RBNode<KVP<String, String>>>,
+        tx: TransactionContext,
+        isRoot: Boolean = false
+    ) {
+        val rootCase = if (isRoot) {
+            ":ROOT"
+        } else {
+            ""
+        }
+        with(nodeView) {
+            tx.run(
+                "MERGE (n: RBNode" + rootCase + "{key : \"${node.elem.key}\", " +
+                        "value: \"${node.elem.v ?: ""}\", " +
+                        "x: $x, y: $y, " +
+                        "isBlack: ${node.col == RBNode.Colour.BLACK}})"
+            )
+            l?.let {
+                exportRBNode(it, tx)
+                tx.run(
+                    "MATCH" +
+                            "  (p:RBNode {key: \"${node.elem.key}\"}), " +
+                            "  (c:RBNode {key: \"${it.node.elem.key}\"}) " +
+                            "MERGE (p)-[:LEFT_CHILD]->(c)"
+                )
+            }
+            r?.let {
+                exportRBNode(it, tx)
+                tx.run(
+                    "MATCH" +
+                            "  (p:RBNode {key: \"${node.elem.key}\"}), " +
+                            "  (c:RBNode {key: \"${it.node.elem.key}\"}) " +
+                            "MERGE (p)-[:RIGHT_CHILD]->(c)"
+                )
+            }
+        }
+    }
 
     fun open(uri: String, username: String, password: String) {
         try {
