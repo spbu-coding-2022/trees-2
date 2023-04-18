f826a70a9f8392e74fdd95bc569679a82c2a22ce
David <parzivalwasd@gmail.com>
2023-04-19 01:01:44 +0300

    fix(NodeView): Change type of KVP + small refactor

--- app/src/main/kotlin/org/tree/app/view/NodeView.kt
+++ app/src/main/kotlin/org/tree/app/view/NodeView.kt
@@ -3,16 +3,14 @@ package org.tree.app.view
 import org.tree.binaryTree.KVP
 import org.tree.binaryTree.templates.TemplateNode
 
-class NodeView<NODE_T : TemplateNode<KVP<String, String>, NODE_T>> { // it is just sketch for import/export
-    var node: NODE_T
+class NodeView<NODE_T : TemplateNode<KVP<Int, String>, NODE_T>>(nd: NODE_T) { // it is just sketch for import/export
+    var node: NODE_T = nd
     var x: Double = 0.0
     var y: Double = 0.0
     var l: NodeView<NODE_T>? = null
     var r: NodeView<NODE_T>? = null
-    //var colour: String = "#FFFFFF"
 
-    constructor(nd: NODE_T) {
-        node = nd
+    init {
         node.left?.let {
             l = NodeView(it)
         }
