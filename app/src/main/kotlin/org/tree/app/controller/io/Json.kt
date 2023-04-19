2dec0ea701e099928769a8dc0eaad1fdfd3387d3
Anna-er <brungilda364@gmail.com>
2023-04-19 04:24:55 +0300

    feat(Json): Add JsonAVLNode and JsonAVLTree data class

--- app/src/main/kotlin/org/tree/app/controller/io/Json.kt
+++ app/src/main/kotlin/org/tree/app/controller/io/Json.kt
@@ -1,4 +1,21 @@
 package org.tree.app.controller.io
 
+import kotlinx.serialization.Serializable
+
+@Serializable
+private data class JsonAVLNode(
+    val key: Int,
+    val value: String?,
+    val x: Double,
+    val y: Double,
+    val height: Int,
+    val lkey: Int?,
+    val rkey: Int?
+)
+
+@Serializable
+private data class JsonAVLTree(
+    val AVLTree: Array<JsonAVLNode?>
+)
 class Json {
 }
