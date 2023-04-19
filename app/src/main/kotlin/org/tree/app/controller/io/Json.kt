9928580bfd1f6f18b75909b0069ddf474fc33747
Anna-er <brungilda364@gmail.com>
2023-04-19 04:41:58 +0300

    feat(Json): Add sketch of exportAVLTree()

--- app/src/main/kotlin/org/tree/app/controller/io/Json.kt
+++ app/src/main/kotlin/org/tree/app/controller/io/Json.kt
@@ -1,6 +1,16 @@
 package org.tree.app.controller.io
 
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.neo4j.driver.exceptions.value.Uncoercible
+import org.tree.app.view.NodeView
+import org.tree.binaryTree.AVLNode
+import org.tree.binaryTree.KVP
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
 
 @Serializable
 private data class JsonAVLNode(
@@ -18,4 +28,47 @@ private data class JsonAVLTree(
     val AVLTree: Array<JsonAVLNode?>
 )
 class Json {
+    val dirPath = "json-saved"
+
+    fun exportAVLTree(root: NodeView<AVLNode<KVP<Int, String>>>) {
+        val sb = StringBuilder()
+        sb.append("{\"AVLTree\":[")
+        traverseExportAVLNode(sb, root)
+        sb.append("]}")
+        File(dirPath).mkdirs()
+        File(dirPath, "output.json").run {
+            createNewFile()
+            writeText(sb.toString())
+        }
+    }
+
+    private fun traverseExportAVLNode(
+        sb: StringBuilder,
+        nodeView: NodeView<AVLNode<KVP<Int, String>>>,
+    ) {
+
+        with(nodeView) {
+            val json = JsonAVLNode(
+                key = node.elem.key,
+                value = node.elem.v,
+                x = x,
+                y = y,
+                height = node.height,
+                lkey = l?.node?.elem?.key,
+                rkey = r?.node?.elem?.key
+            )
+
+            sb.append(Json.encodeToString(json))
+
+            l?.let {
+                sb.append(",")
+                traverseExportAVLNode(sb, it)
+            }
+            r?.let {
+                sb.append(",")
+                traverseExportAVLNode(sb, it)
+            }
+        }
+
+    }
 }
