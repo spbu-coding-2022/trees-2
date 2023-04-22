2bdbcb164a3f621bb23839d017a22094450ef3fb
David <parzivalwasd@gmail.com>
2023-04-22 07:46:05 +0300

    refactor(RBTree): Add algorithm source in comments

--- binaryTree/src/main/kotlin/org/tree/binaryTree/trees/RBTree.kt
+++ binaryTree/src/main/kotlin/org/tree/binaryTree/trees/RBTree.kt
@@ -2,7 +2,7 @@ package org.tree.binaryTree.trees
 
 import org.tree.binaryTree.RBNode
 import org.tree.binaryTree.templates.TemplateBalanceBSTree
-
+// algorithm source: https://www.youtube.com/watch?v=T70nn4EyTrs&ab_channel=%D0%9B%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%B8%D0%B9%D0%A4%D0%9F%D0%9C%D0%98
 class RBTree<T : Comparable<T>> : TemplateBalanceBSTree<T, RBNode<T>>() {
     private fun findParentForNewNode(curNode: RBNode<T>?, obj: T): RBNode<T>? {
         if (curNode != null) {
