72afe5a837ef9faf22d5e8787b84646c55138181
Efremov Alexey <dragonmail135159@gmail.com>
2023-04-23 15:47:42 +0300

    fix: Add lost dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-core:0.40.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.40.1")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.40.1")
+
+    implementation(compose.desktop.currentOs)
 }
 
 application {
