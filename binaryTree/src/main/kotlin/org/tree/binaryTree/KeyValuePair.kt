6edb7312ddfb2554adb61095bc8ee4563d79719e
David <parzivalwasd@gmail.com>
2023-04-23 16:01:24 +0300

    feat(KVP): Make KVP data class

--- binaryTree/src/main/kotlin/org/tree/binaryTree/KeyValuePair.kt
+++ binaryTree/src/main/kotlin/org/tree/binaryTree/KeyValuePair.kt
@@ -1,6 +1,6 @@
 package org.tree.binaryTree
 
-class KVP<K : Comparable<K>, V>(val key: K, var v: V? = null) : Comparable<KVP<K, V>> {
+data class KVP<K : Comparable<K>, V>(val key: K, var v: V? = null) : Comparable<KVP<K, V>> {
     override fun compareTo(other: KVP<K, V>): Int {
         return key.compareTo(other.key)
     }
