4170013166a1aa8f83ddf048b3d893cb40f6f0ac
Efremov Alexey <dragonmail135159@gmail.com>
2023-04-24 19:10:51 +0300

    feat: Add alert dialog for exceptions

---
+++ app/src/main/kotlin/org/tree/app/view/dialogs/io/ExceptionIOHandler.kt
@@ -0,0 +1,48 @@
+package org.tree.app.view.dialogs.io
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.rememberDialogState
+import org.tree.app.controller.io.HandledIOException
+
+fun <T> handleIOException(onCatch: (HandledIOException) -> Unit, handledCode: () -> T): T? {
+    try {
+        return handledCode()
+    } catch (ex: HandledIOException) {
+        onCatch(ex)
+        return null
+    }
+}
+
+@Composable
+fun AlertDialog(description: String, isOpen: Boolean, onCloseRequest: () -> Unit) {
+    if (isOpen) {
+        Dialog(state = rememberDialogState(width = 500.dp, height = 250.dp),
+            title = "Something go wrong...",
+            onCloseRequest = {
+                onCloseRequest()
+            }) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                Spacer(modifier = Modifier.weight(0.1f))
+                Row {
+                    Spacer(modifier = Modifier.weight(0.1f))
+                    Text(description, modifier = Modifier.weight(1.0f))
+                    Spacer(modifier = Modifier.weight(0.1f))
+                }
+
+                Spacer(modifier = Modifier.weight(0.1f))
+                Button(onClick = { onCloseRequest() }) {
+                    Text("Ok")
+                }
+            }
+        }
+    }
+}
