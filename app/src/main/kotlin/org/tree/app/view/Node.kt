da152a23343c79b69a82f5cd53a1ae82cdf9fe97
Efremov Alexey <dragonmail135159@gmail.com>
2023-04-24 21:30:39 +0300

    fix: Fix moment than all tree disappears

--- app/src/main/kotlin/org/tree/app/view/Node.kt
+++ app/src/main/kotlin/org/tree/app/view/Node.kt
@@ -11,12 +11,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.PointerInputChange
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
@@ -43,9 +44,10 @@ class NodeView<NODE_T : TemplateNode<KVP<Int, String>, NODE_T>>(nd: NODE_T) { //
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Node(
-    x: MutableState<Int>,
-    y: MutableState<Int>,
+    x: Int,
+    y: Int,
     key: Int, value: String,
+    onDrag: (PointerInputChange, Offset) -> Unit,
     modifier: Modifier = Modifier,
     color: Color = Color.Gray,
     size: Int = 10,
@@ -64,7 +66,7 @@ fun Node(
                     )
                 }
             },
-            modifier = Modifier.offset((x.value - size / 2).dp, (y.value - size / 2).dp),
+            modifier = Modifier.offset((x - size / 2).dp, (y - size / 2).dp),
             delayMillis = 600,
             tooltipPlacement = TooltipPlacement.CursorPoint(
                 alignment = Alignment.BottomEnd,
@@ -75,11 +77,9 @@ fun Node(
                 contentAlignment = Alignment.Center,
                 modifier = modifier
                     .size(size.dp).clip(CircleShape).background(color = color)
-                    .pointerInput(x, y) {
+                    .pointerInput(onDrag) {
                         detectDragGestures { change, dragAmount ->
-                            change.consume()
-                            x.value += dragAmount.x.toInt()
-                            y.value += dragAmount.y.toInt()
+                            onDrag(change, dragAmount)
                         }
                     }
             ) {
