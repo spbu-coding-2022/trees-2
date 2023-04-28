defdfdaa9b8e5892428e35594233f19d16ea1644
David <parzivalwasd@gmail.com>
2023-04-28 18:29:37 +0300

    feat(FilesIOHandler): Add Import and Export of SQLite files in UI

---
+++ app/src/main/kotlin/org/tree/app/view/dialogs/io/FilesIOHandler.kt
@@ -0,0 +1,38 @@
+package org.tree.app.view.dialogs.io
+
+import TreeController
+import androidx.compose.ui.awt.ComposeWindow
+import org.tree.app.controller.io.SQLiteIO
+import org.tree.binaryTree.KVP
+import org.tree.binaryTree.Node
+import java.awt.FileDialog
+import java.io.File
+
+fun importBST(
+    window: ComposeWindow,
+): TreeController<Node<KVP<Int, String>>>? {
+    val fileString = selectFile(window, "sqlite") ?: return null
+    val file = File(fileString)
+    val db = SQLiteIO()
+    return db.importTree(file)
+}
+
+fun exportBST(window: ComposeWindow, tc: TreeController<Node<KVP<Int, String>>>) {
+    val fileString = selectFile(window, "sqlite") ?: return
+    val file = File(fileString)
+    val db = SQLiteIO()
+    db.exportTree(tc, file)
+}
+
+fun selectFile(window: ComposeWindow, fileFormant: String): String? {
+    val fd = FileDialog(window, "Choose a file", FileDialog.LOAD)
+    fd.directory = "C:\\"
+    fd.file = "*.$fileFormant"
+    fd.isVisible = true
+    val fileString = fd.directory + fd.file
+    if (fileString != "nullnull") {
+        return fd.directory + fd.file
+    }
+    return null
+}
+
