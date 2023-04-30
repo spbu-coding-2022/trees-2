8e792d88099b215f8aa2ad0df5ea072f95acfbf8
David <parzivalwasd@gmail.com>
2023-04-30 19:07:47 +0300

    feat(App, FilesIOHandler): Improve import methods: now it's using caught exceptions

--- app/src/main/kotlin/org/tree/app/view/dialogs/io/FilesIOHandler.kt
+++ app/src/main/kotlin/org/tree/app/view/dialogs/io/FilesIOHandler.kt
@@ -3,7 +3,10 @@ package org.tree.app.view.dialogs.io
 import TreeController
 import androidx.compose.ui.awt.ComposeWindow
 import org.tree.app.appDataController
-import org.tree.app.controller.io.*
+import org.tree.app.controller.io.Json
+import org.tree.app.controller.io.SQLiteIO
+import org.tree.app.controller.io.SavedTree
+import org.tree.app.controller.io.SavedType
 import org.tree.binaryTree.AVLNode
 import org.tree.binaryTree.KVP
 import org.tree.binaryTree.Node
@@ -13,7 +16,7 @@ import java.io.File
 fun importAVLT(
     window: ComposeWindow,
 ): TreeController<AVLNode<KVP<Int, String>>>? {
-    val fileString = selectFile(window, "json", "import") ?: return null
+    val fileString = selectFile(window, "json", Mode.IMPORT) ?: return null
     val file = File(fileString)
     val db = Json()
     val treeController = db.importTree(file)
@@ -23,7 +26,7 @@ fun importAVLT(
 }
 
 fun exportAVLT(window: ComposeWindow, tc: TreeController<AVLNode<KVP<Int, String>>>) {
-    val fileString = selectFile(window, "json", "export") ?: return
+    val fileString = selectFile(window, "json", Mode.EXPORT) ?: return
     val file = File(fileString)
     val db = Json()
     db.exportTree(tc, file)
@@ -34,23 +37,17 @@ fun exportAVLT(window: ComposeWindow, tc: TreeController<AVLNode<KVP<Int, String
 fun importBST(
     window: ComposeWindow,
 ): TreeController<Node<KVP<Int, String>>>? {
-    val fileString = selectFile(window, "sqlite", "import") ?: return null
+    val fileString = selectFile(window, "sqlite", Mode.IMPORT) ?: return null
     val file = File(fileString)
     val db = SQLiteIO()
-    val treeController = try {
-        db.importTree(file)
-    } catch (ex: HandledIOException) {
-        null
-    }
-    if (treeController != null) {
-        appDataController.data.lastTree = SavedTree(SavedType.SQLite, file.path)
-        appDataController.saveData()
-    }
+    val treeController = db.importTree(file)
+    appDataController.data.lastTree = SavedTree(SavedType.SQLite, file.path)
+    appDataController.saveData()
     return treeController
 }
 
 fun exportBST(window: ComposeWindow, tc: TreeController<Node<KVP<Int, String>>>) {
-    val fileString = selectFile(window, "sqlite", "export") ?: return
+    val fileString = selectFile(window, "sqlite", Mode.EXPORT) ?: return
     val file = File(fileString)
     val db = SQLiteIO()
     db.exportTree(tc, file)
@@ -58,8 +55,13 @@ fun exportBST(window: ComposeWindow, tc: TreeController<Node<KVP<Int, String>>>)
     appDataController.saveData()
 }
 
-fun selectFile(window: ComposeWindow, fileFormant: String, mode: String): String? {
-    val fd = if (mode == "import") {
+enum class Mode {
+    IMPORT,
+    EXPORT
+}
+
+fun selectFile(window: ComposeWindow, fileFormant: String, mode: Mode): String? {
+    val fd = if (mode == Mode.IMPORT) {
         FileDialog(window, "Choose .sqlite file to import", FileDialog.LOAD)
     } else {
         FileDialog(window, "Choose .sqlite file to export", FileDialog.SAVE)
@@ -73,4 +75,3 @@ fun selectFile(window: ComposeWindow, fileFormant: String, mode: String): String
     }
     return null
 }
-
