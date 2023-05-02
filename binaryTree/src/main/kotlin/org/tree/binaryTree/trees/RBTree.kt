766473789b1a17b07d5342d796b1ef3241395a19
David <parzivalwasd@gmail.com>
2023-05-02 23:12:26 +0300

    refactor(RBTree): Remove extra `return`'s

--- binaryTree/src/main/kotlin/org/tree/binaryTree/trees/RBTree.kt
+++ binaryTree/src/main/kotlin/org/tree/binaryTree/trees/RBTree.kt
@@ -324,7 +324,6 @@ class RBTree<T : Comparable<T>> : TemplateBalanceBSTree<T, RBNode<T>>() {
                 balanceRemove2(grandParent, BalanceCase.ChangedChild.RIGHT)
             }
         }
-        return
     }
 
 
@@ -358,7 +357,6 @@ class RBTree<T : Comparable<T>> : TemplateBalanceBSTree<T, RBNode<T>>() {
                 balanceRemove2(grandParent, BalanceCase.ChangedChild.RIGHT)
             }
         }
-        return
     }
 
     private fun getColourOfRemovedNode(parentNode: RBNode<T>): RBNode.Colour {
