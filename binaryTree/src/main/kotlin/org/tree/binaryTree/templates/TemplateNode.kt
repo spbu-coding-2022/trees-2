07112021dc0185fea901121a28dd243368edf226
Efremov Alexey <dragonmail135159@gmail.com>
2023-05-02 17:18:38 +0300

    refactor: Add kdoc to TemplateNode

--- binaryTree/src/main/kotlin/org/tree/binaryTree/templates/TemplateNode.kt
+++ binaryTree/src/main/kotlin/org/tree/binaryTree/templates/TemplateNode.kt
@@ -1,7 +1,15 @@
 package org.tree.binaryTree.templates
 
-abstract class TemplateNode<T : Comparable<T>, NODE_T : TemplateNode<T, NODE_T>>(v: T) {
-    var elem: T = v
+/**
+ * This class is template class for creating your own nodes.
+ * @param T the type of element stored in the node
+ * @param NODE_T the type of your node
+ *
+ * @property elem the element stored in the node.
+ * @property left the left child node of this node, or null if this node does not have a left child node.
+ * @property right the right child node of this node, or null if this node does not have a right child node.
+ */
+abstract class TemplateNode<T : Comparable<T>, NODE_T : TemplateNode<T, NODE_T>>(var elem: T) {
     var left: NODE_T? = null
     var right: NODE_T? = null
 
@@ -27,30 +35,38 @@ abstract class TemplateNode<T : Comparable<T>, NODE_T : TemplateNode<T, NODE_T>>
         return res
     }
 
-    //Traversal
+    /**
+     * @property INORDER first the left child, then the parent and the right child
+     * @property PREORDER first the parent, then the left child and the right child
+     * @property POSTORDER first the left child, then the right child and the parent
+     */
     enum class Traversal {
         INORDER,
         PREORDER,
         POSTORDER
     }
 
-    private fun traversal(res: MutableList<T>, cs: Traversal) {
-        if (cs == Traversal.PREORDER) {
+    private fun traverse(res: MutableList<T>, traversalOrder: Traversal) {
+        if (traversalOrder == Traversal.PREORDER) {
             res.add(elem)
         }
-        left?.traversal(res, cs)
-        if (cs == Traversal.INORDER) {
+        left?.traverse(res, traversalOrder)
+        if (traversalOrder == Traversal.INORDER) {
             res.add(elem)
         }
-        right?.traversal(res, cs)
-        if (cs == Traversal.POSTORDER) {
+        right?.traverse(res, traversalOrder)
+        if (traversalOrder == Traversal.POSTORDER) {
             res.add(elem)
         }
     }
 
-    fun traversal(order: Traversal): MutableList<T> {
+    /**
+     * @return a list of nodes' elements in the specified order
+     * @param order specified traversal order
+     */
+    fun traverse(order: Traversal): MutableList<T> {
         val res: MutableList<T> = mutableListOf()
-        traversal(res, order)
+        traverse(res, order)
         return res
     }
 }
