74e5866f54cb2bbd93cbc5ced35b5fe6bfc9043b
Efremov Alexey <dragonmail135159@gmail.com>
2023-05-02 21:10:19 +0300

    fix: Remove legacy code

--- app/src/main/kotlin/org/tree/app/view/Node.kt
+++ app/src/main/kotlin/org/tree/app/view/Node.kt
@@ -22,25 +22,6 @@ import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.tree.binaryTree.KVP
-import org.tree.binaryTree.templates.TemplateNode
-
-class NodeView<NODE_T : TemplateNode<KVP<Int, String>, NODE_T>>(nd: NODE_T) { // legacy code
-    var node: NODE_T = nd
-    var x: Double = 0.0
-    var y: Double = 0.0
-    var l: NodeView<NODE_T>? = null
-    var r: NodeView<NODE_T>? = null
-
-    init {
-        node.left?.let {
-            l = NodeView(it)
-        }
-        node.right?.let {
-            r = NodeView(it)
-        }
-    }
-}
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
