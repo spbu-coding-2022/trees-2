5a4470321d6151f6f0e200d437cb10ce090a8597
David <parzivalwasd@gmail.com>
2023-05-02 23:22:48 +0300

    refactor(RBTree): Optimize `getBlackHeight` method

--- binaryTree/src/main/kotlin/org/tree/binaryTree/trees/RBTree.kt
+++ binaryTree/src/main/kotlin/org/tree/binaryTree/trees/RBTree.kt
@@ -368,14 +368,17 @@ class RBTree<T : Comparable<T>> : TemplateBalanceBSTree<T, RBNode<T>>() {
     }
 
     private fun getBlackHeight(curNode: RBNode<T>?, blackHeight: Int = 0): Int {
-        var blackHeightVar = blackHeight
         if (curNode != null) {
-            if (curNode.col == RBNode.Colour.BLACK) {
-                blackHeightVar += 1
-            }
-            return getBlackHeight(curNode.left, blackHeightVar)
+            return getBlackHeight(
+                curNode.left,
+                if (curNode.col == RBNode.Colour.BLACK) {
+                    blackHeight + 1
+                } else {
+                    blackHeight
+                }
+            )
         }
-        return blackHeightVar
+        return blackHeight
     }
 
     override fun replaceNode(replacedNode: RBNode<T>, parentNode: RBNode<T>?, newNode: RBNode<T>?) {
